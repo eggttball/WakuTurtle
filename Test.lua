45e09c76a62cea13799cf1cd10180a2e1709e467
蛋踢球 <eggttball@gmail.com>
2024-09-17 11:19:42 +0800

    test: keepDigging()

--- Test.lua
+++ Test.lua
@@ -1,8 +1,4 @@
 require "WakuTurtle"
 
-Length = 6
-Weight = 6
-Height = 6
-
-local builder = WakuTurtle:new("Ant", turtle, Length, Weight, Height, -2, 0, -1)
-builder:dig()
+local builder = WakuTurtle:new("Ant", turtle, Length, Weight, Height)
+builder:keepDigging()
