b356b2fcbc6a503ab9d93410a42cb8b69b3ec1b7
蛋踢球 <eggttball@gmail.com>
2024-09-19 00:43:24 +0800

    feat: Change digging direction from right->left to bottom->up for flexible design

--- SmartCraft.lua
+++ SmartCraft.lua
@@ -10,8 +10,7 @@ local builder = WakuTurtle:new("Ant", turtle, length, weight, height)
 builder:log()
 
 -- 初始位置必須是在儲物箱正上方
--- 接著往前一格，再移動到最右邊開始作業
-builder:dig()
+-- 再移動到最右邊開始作業
 builder:digAuto(DIR.RGT, math.floor(builder:getWeight() / 2) + rightShift)
 
 -- 若水平線位置為負數，則從下方開始挖
@@ -19,37 +18,40 @@ if horizontal < 0 then
     builder:digAuto(DIR.DWN, math.abs(horizontal))
 end
 
-local len = 0
-local direction = DIR.UP
-while len < builder:getLength() do
+local hgt = 0
+while hgt < builder:getHeight() do
 
-    if len % 4 == 1 and direction == DIR.DWN then
-        builder:placeTorch()
-    end
+    -- 往前挖掘一整排的方塊
+    builder:digAuto(DIR.FWD, builder:getLength() + 1)
 
-    -- 往上挖掘一排高的方塊
     local wgt = 1
-    builder:digAuto(direction, builder:getHeight() - 1)
-    direction = direction * -1
-
-    -- 接下來往下挖掘一排、再往上挖掘一排，持續上下交替直到寬度到達 Weight
+    -- 接下來由右向左持續來回挖掘，直到寬度到達 Weight
     while wgt < builder:getWeight() do
-        builder:digAuto(DIR.LFT, 1)
-        builder:digAuto(direction, builder:getHeight() - 1)
-        direction = direction * -1
+        if builder.facing == DIR.FWD then
+            builder:digAuto(DIR.LFT, 1)
+        elseif builder.facing == DIR.BCK then
+            builder:digAuto(DIR.RGT, 1)
+        end
+
+        builder:turnLeft()
+        builder:turnLeft()
+        builder:digAuto(DIR.FWD, builder:getLength() + 1)
         wgt = wgt + 1
     end
 
-    -- 往前挖掘一格後再回到最右邊初始位置
-    len = len + 1
-    if len < builder:getLength() then
-        builder:dig()
+    -- 往上挖掘一格後再回到最右邊位置
+    hgt = hgt + 1
+    if hgt < builder:getHeight() then
+        builder:dig(DIR.UP)
 
-        if len % 4 == 1 and direction == DIR.DWN then
-            builder:placeTorch()
+        if builder.facing == DIR.FWD then
+            builder:digAuto(DIR.RGT, builder:getWeight() - 1)
+        elseif builder.facing == DIR.BCK then
+            builder:digAuto(DIR.LFT, builder:getWeight() - 1)
         end
 
-        builder:digAuto(DIR.RGT, builder:getWeight() - 1)
+        builder:turnLeft()
+        builder:turnLeft()
     end
 end
 
