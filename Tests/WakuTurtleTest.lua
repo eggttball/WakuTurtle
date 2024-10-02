da6452ec65c79925537913732ed37143f9d9a937
蛋踢球 <eggttball@gmail.com>
2024-10-02 20:46:25 +0800

    test: Unit test for WakuTurtle dig mode and repeat x or repeat y

--- Tests/WakuTurtleTest.lua
+++ Tests/WakuTurtleTest.lua
@@ -38,4 +38,46 @@ function TestWakuTurtle:testIsReserveSpace_DigMode_NoRepeat()
 end
 
 
+function TestWakuTurtle:testIsReserveSpace_DigMode_RepeatX()
+    local len, wgt, hgt = 3, 7, 5
+    local shift_x, shift_y = -2, -1
+    local name = "Dig with REPEAT X, start at " .. shift_x .. ", " .. shift_y
+    local turtle = WakuTurtle:new(name, nil, BUILD_MODE.DIG, REPEAT_MODE.REPEAT, REPEAT_MODE.NO_REPEAT, len, wgt, hgt, shift_x, shift_y)
+    turtle:assignReserveBlocks(reserveBlocks)
+    turtle:printReserveBlocks()
+    turtle.pos.x = 2   turtle.pos.y = -1
+    luaunit.assertTrue(turtle:isReserveSpace())
+    turtle.pos.x = 3   turtle.pos.y = 0
+    luaunit.assertFalse(turtle:isReserveSpace())
+    turtle.pos.x = 4   turtle.pos.y = 2
+    luaunit.assertTrue(turtle:isReserveSpace())
+    turtle.pos.x = -2   turtle.pos.y = 3
+    luaunit.assertFalse(turtle:isReserveSpace())
+    turtle.pos.x = 1   turtle.pos.y = 3
+    luaunit.assertFalse(turtle:isReserveSpace())
+    turtle.pos.x = 3   turtle.pos.y = 3
+    luaunit.assertFalse(turtle:isReserveSpace())
+end
+
+
+function TestWakuTurtle:testIsReserveSpace_DigMode_RepeatY()
+    local len, wgt, hgt = 3, 7, 11
+    local shift_x, shift_y = -2, -1
+    local name = "Dig with REPEAT Y, start at " .. shift_x .. ", " .. shift_y
+    local turtle = WakuTurtle:new(name, nil, BUILD_MODE.DIG, REPEAT_MODE.NO_REPEAT, REPEAT_MODE.REPEAT, len, wgt, hgt, shift_x, shift_y)
+    turtle:assignReserveBlocks(reserveBlocks)
+    turtle:printReserveBlocks()
+    turtle.pos.x = 2   turtle.pos.y = -1
+    luaunit.assertFalse(turtle:isReserveSpace())
+    turtle.pos.x = 4   turtle.pos.y = 2
+    luaunit.assertFalse(turtle:isReserveSpace())
+    turtle.pos.x = -2   turtle.pos.y = 3
+    luaunit.assertTrue(turtle:isReserveSpace())
+    turtle.pos.x = 1   turtle.pos.y = 4
+    luaunit.assertTrue(turtle:isReserveSpace())
+    turtle.pos.x = -1   turtle.pos.y = 9
+    luaunit.assertTrue(turtle:isReserveSpace())
+end
+
+
 os.exit(luaunit.LuaUnit.run())
