9065780c72a0b16fdedc2713e15a267f4b8c7bae
蛋踢球 <eggttball@gmail.com>
2024-10-06 11:08:48 +0800

    Update test script

--- Tests/Test.lua
+++ Tests/Test.lua
@@ -3,7 +3,7 @@ require "WakuTurtle"
 builder = WakuTurtle:new("Ant", turtle, BUILD_MODE.FILL, REPEAT_MODE.MIRROR, REPEAT_MODE.REPEAT, 1, 24, 9, -11, 0)
 builder:gotoStartPos()
 
-wgt, hgt, step = 0, 0, 0
+step = 1
 dir, facing = DIR.EAST, builder.facing
 nextActionOnDir = function () builder:faceTo(dir) builder:digAuto(POS.FWD, 1) end
 
@@ -11,14 +11,14 @@ nextX, nextY, nextFacing = builder:getNextWorkingPos(dir)
 
 
 
-if nextX > builder.pos.x then step = 1 elseif nextX < builder.pos.x then step = -1 end
-if nextX ~= builder.pos.x then if step < 0 then dir = DIR.WEST end for i = builder.pos.x, nextX - step, step do nextActionOnDir() end end
+if nextX > builder.pos.x then step = 1; dir = DIR.EAST; elseif nextX < builder.pos.x then step = -1; dir = DIR.WEST; end
 
-if nextY ~= builder.pos.y then for i = builder.pos.y, nextY - 1, 1 do hgt = hgt + 1 builder:dig(POS.UP) end end
-dir = nextFacing
+for i = builder.pos.x, nextX - step, step do nextActionOnDir() end
+for i = builder.pos.y, nextY - 1, 1 do builder:dig(POS.UP) end
+dir = nextFacing; step = (dir == DIR.EAST) and 1 or -1
 
 builder:faceTo(POS.getRevDir(facing)) builder:placeAuto(POS.FWD, builder:getLength())
-wgt, facing = wgt + 1, DIR.getRevDir(facing)
+facing = DIR.getRevDir(facing)
 
 nextX, nextY, nextFacing = builder:getNextWorkingPos(dir, builder.pos.x + step, builder.pos.y)
 if nextY == builder.pos.y then nextActionOnDir() elseif nextX > builder.pos.x then dir = DIR.EAST nextActionOnDir() elseif nextX < builder.pos.x then dir = DIR.WEST nextActionOnDir() end
